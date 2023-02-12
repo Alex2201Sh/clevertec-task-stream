c251c57dfbeb70f9ddda918a5eb42d709f34b753
Alex2201Sh <shumilov218@gmail.com>
2023-02-12 12:49:20 +0300

    Added project files.

---
+++ src/main/java/by/shumilov/Main.java
@@ -0,0 +1,111 @@
+package by.shumilov;
+
+import by.shumilov.model.*;
+import by.shumilov.util.Util;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+        task11();
+        task12();
+        task13();
+        task14();
+        task15();
+    }
+
+    private static void task1() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task2() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+
+    }
+
+    private static void task3() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task4() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+
+    }
+
+    private static void task5() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task6() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task7() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task8() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task9() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task10() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task11() throws IOException {
+        List<Animal> animals = Util.getAnimals();
+        //        animals.stream() Продолжить ...
+    }
+
+    private static void task12() throws IOException {
+        List<Person> people = Util.getPersons();
+//        Продолжить...
+    }
+
+    private static void task13() throws IOException {
+        List<House> houses = Util.getHouses();
+        //        Продолжить...
+    }
+
+    private static void task14() throws IOException {
+        List<Car> cars = Util.getCars();
+        //        Продолжить...
+    }
+
+    private static void task15() throws IOException {
+        List<Flower> flowers = Util.getFlowers();
+        //        Продолжить...
+    }
+}
